86cc94ef59911561f6da7f1488f51e5153551481
wnortier <warwick.nortier@gmail.com>
2021-01-31 01:50:34 +0200

    finished animations and added btn

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("sanity check2");
+var container = document.getElementById("container");
+var text = document.getElementById("text");
+var btn = document.getElementById("btn");
+var pointerContainer = document.querySelector(".pointer-container");
+var totalTime = 7500;
+var breathTime = (totalTime / 5) * 2;
+var holdTime = totalTime / 5;
+var running = false;
+btn.style.opacity = "1";
+btn.addEventListener("click", function () {
+    btn.style.opacity = "0";
+    btn.style.animation = "disappear 1s";
+    pointerContainer.style.top = "-40px";
+    pointerContainer.style.left = "140px";
+    var breathAnimation = function () {
+        text.innerText = "Breath In!";
+        container.className = "container grow";
+        setTimeout(function () {
+            text.innerText = "Hold!";
+            setTimeout(function () {
+                text.innerText = "Breath Out!";
+                container.className = "container shrink";
+            }, holdTime);
+        }, breathTime);
+    };
+    pointerContainer.style.animation = "rotate 7.5s linear forwards infinite";
+    breathAnimation();
+    setInterval(breathAnimation, totalTime);
 });
--- script.ts
+++ script.ts
@@ -1,3 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("sanity check");
+const container = document.getElementById("container") as HTMLDivElement;
+const text = document.getElementById("text") as HTMLParagraphElement;
+const btn = document.getElementById("btn") as HTMLButtonElement;
+const pointerContainer = document.querySelector(
+  ".pointer-container"
+) as HTMLButtonElement;
+
+const totalTime = 7500;
+const breathTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+const running = false;
+btn.style.opacity = "1";
+
+btn.addEventListener("click", () => {
+  btn.style.opacity = "0";
+  btn.style.animation = "disappear 1s";
+  pointerContainer.style.top = "-40px";
+  pointerContainer.style.left = "140px";
+  const breathAnimation = (): any => {
+    text.innerText = "Breath In!";
+    container.className = "container grow";
+    setTimeout(() => {
+      text.innerText = "Hold!";
+      setTimeout(() => {
+        text.innerText = "Breath Out!";
+        container.className = "container shrink";
+      }, holdTime);
+    }, breathTime);
+  };
+  pointerContainer.style.animation = "rotate 7.5s linear forwards infinite";
+  breathAnimation();
+  setInterval(breathAnimation, totalTime);
 });
